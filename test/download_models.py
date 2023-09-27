9c46e162e1361aff37f64c294452144262313b81
Garry435 <143331750+Garry435@users.noreply.github.com>
2023-09-27 15:48:23 +0530

    Add files via upload

---
+++ test/download_models.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import requests
+
+MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
+VoiceC_DOWNLOAD_LINK = 'https://huggingface.co/Garry908/sample-test/resolve/main/'
+
+mdxnet_models_dir = 'mdxnet_models'
+
+def dl_model(link, model_name):
+    if "huggingface" not in link:
+        linkname = model_name.split('/')[1]
+    else:
+        linkname = model_name
+    with requests.get(f'{link}{linkname}') as r:
+        r.raise_for_status()
+        with open(f"{model_name}", 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
+if __name__ == '__main__':
+    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
+    for model in mdx_model_names:
+        print(f'Downloading {model}...')
+        dl_model(MDX_DOWNLOAD_LINK, f"{mdxnet_models_dir}/{model}")
+
+    VoiceC_model_names = ['hubert_base.pt', 'rmvpe.pt']
+    for model in VoiceC_model_names:
+        print(f'Downloading {model}...')
+        dl_model(VoiceC_DOWNLOAD_LINK, model)
+
+    print('All models downloaded!')
---
+++ test/main.py
@@ -0,0 +1,287 @@
+from multiprocessing import cpu_count
+from pathlib import Path
+from pydub import AudioSegment
+import torch
+from fairseq import checkpoint_utils
+import gc
+import hashlib
+import json
+import librosa
+import shlex
+import subprocess
+from contextlib import suppress
+from urllib.parse import urlparse, parse_qs
+import sox
+import yt_dlp
+import numpy as np
+from pedalboard.io import AudioFile
+from pydub import AudioSegment
+from mdx import run_mdx
+import sox
+from scipy.io import wavfile
+from infer_pack.models import (
+    SynthesizerTrnMs256NSFsid,
+    SynthesizerTrnMs256NSFsid_nono,
+    SynthesizerTrnMs768NSFsid,
+    SynthesizerTrnMs768NSFsid_nono,
+)
+from my_utils import load_audio
+from vc_infer_pipeline import VC
+import requests,os
+from zipfile import ZipFile
+
+
+class Config:
+    def __init__(self, device, is_half):
+        self.device = device
+        self.is_half = is_half
+        self.n_cpu = 0
+        self.gpu_name = None
+        self.gpu_mem = None
+        self.x_pad, self.x_query, self.x_center, self.x_max = self.device_config()
+
+    def device_config(self) -> tuple:
+        if torch.cuda.is_available():
+            i_device = int(self.device.split(":")[-1])
+            self.gpu_name = torch.cuda.get_device_name(i_device)
+            if (
+                    ("16" in self.gpu_name and "V100" not in self.gpu_name.upper())
+                    or "P40" in self.gpu_name.upper()
+                    or "1060" in self.gpu_name
+                    or "1070" in self.gpu_name
+                    or "1080" in self.gpu_name
+            ):
+                print("16 series/10 series P40 forced single precision")
+                self.is_half = False
+                for config_file in ["32k.json", "40k.json", "48k.json"]:
+                    with open("configs" / config_file, "r") as f:
+                        strr = f.read().replace("true", "false")
+                    with open("configs" / config_file, "w") as f:
+                        f.write(strr)
+                with open("trainset_preprocess_pipeline_print.py", "r") as f:
+                    strr = f.read().replace("3.7", "3.0")
+                with open("trainset_preprocess_pipeline_print.py", "w") as f:
+                    f.write(strr)
+            else:
+                self.gpu_name = None
+            self.gpu_mem = int(
+                torch.cuda.get_device_properties(i_device).total_memory
+                / 1024
+                / 1024
+                / 1024
+                + 0.4
+            )
+            if self.gpu_mem <= 4:
+                with open("trainset_preprocess_pipeline_print.py", "r") as f:
+                    strr = f.read().replace("3.7", "3.0")
+                with open("trainset_preprocess_pipeline_print.py", "w") as f:
+                    f.write(strr)
+        elif torch.backends.mps.is_available():
+            print("No supported N-card found, use MPS for inference")
+            self.device = "mps"
+        else:
+            print("No supported N-card found, use CPU for inference")
+            self.device = "cpu"
+            self.is_half = True
+
+        if self.n_cpu == 0:
+            self.n_cpu = cpu_count()
+
+        if self.is_half:
+            # 6G memory config
+            x_pad = 3
+            x_query = 10
+            x_center = 60
+            x_max = 65
+        else:
+            # 5G memory config
+            x_pad = 1
+            x_query = 6
+            x_center = 38
+            x_max = 41
+
+        if self.gpu_mem != None and self.gpu_mem <= 4:
+            x_pad = 1
+            x_query = 5
+            x_center = 30
+            x_max = 32
+
+        return x_pad, x_query, x_center, x_max
+
+
+def load_hubert(device, is_half, model_path):
+    models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task([model_path], suffix='', )
+    hubert = models[0]
+    hubert = hubert.to(device)
+
+    if is_half:
+        hubert = hubert.half()
+    else:
+        hubert = hubert.float()
+
+    hubert.eval()
+    return hubert
+
+
+def get_vc(device, is_half, config, model_path):
+    cpt = torch.load(model_path, map_location='cpu')
+    if "config" not in cpt or "weight" not in cpt:
+        raise ValueError(f'Incorrect format for {model_path}. Use a voice model trained using RVC v2 instead.')
+
+    tgt_sr = cpt["config"][-1]
+    cpt["config"][-3] = cpt["weight"]["emb_g.weight"].shape[0]
+    if_f0 = cpt.get("f0", 1)
+    version = cpt.get("version", "v1")
+
+    if version == "v1":
+        if if_f0 == 1:
+            net_g = SynthesizerTrnMs256NSFsid(*cpt["config"], is_half=is_half)
+        else:
+            net_g = SynthesizerTrnMs256NSFsid_nono(*cpt["config"])
+    elif version == "v2":
+        if if_f0 == 1:
+            net_g = SynthesizerTrnMs768NSFsid(*cpt["config"], is_half=is_half)
+        else:
+            net_g = SynthesizerTrnMs768NSFsid_nono(*cpt["config"])
+
+    del net_g.enc_q
+    print(net_g.load_state_dict(cpt["weight"], strict=False))
+    net_g.eval().to(device)
+
+    if is_half:
+        net_g = net_g.half()
+    else:
+        net_g = net_g.float()
+
+    vc = VC(tgt_sr, config)
+    return cpt, version, net_g, tgt_sr, vc
+
+
+def svc_infer(index_path, index_rate, input_path, output_path, pitch_change, f0_method, cpt, version, net_g, filter_radius, tgt_sr, rms_mix_rate, protect, crepe_hop_length, vc, hubert_model):
+    audio = load_audio(input_path, 16000)
+    times = [0, 0, 0]
+    if_f0 = cpt.get('f0', 1)
+    audio_opt = vc.pipeline(hubert_model, net_g, 0, audio, input_path, times, pitch_change, f0_method, index_path, index_rate, if_f0, filter_radius, tgt_sr, 0, rms_mix_rate, version, protect, crepe_hop_length)
+    print('Succesfully converted!\nSaving the mp3 file...')
+    wavfile.write(output_path, tgt_sr, audio_opt)
+    
+
+def get_model_path(folder):
+    svc_model_path, svc_index_path = None,None
+    for filee in os.listdir(folder):
+        if filee.endswith('.index'):
+            svc_index_path = folder+'/'+filee
+        if filee.endswith('.pth'):
+            svc_model_path = folder+'/'+filee
+    return svc_model_path, svc_index_path
+
+def extract(filename,output_path=""):
+    with ZipFile(filename,'r') as zObject:
+        zObject.extractall(path=output_path)
+
+def down(url,output_file):
+    r = requests.get(url)
+    with open(output_file,'wb') as f:
+        f.write(r.content)
+
+
+def convert_to_stereo(audio_path):
+    wave, sr = librosa.load(audio_path, mono=False, sr=44100)
+
+    # check if mono
+    if type(wave[0]) != np.ndarray:
+        stereo_path = f'{os.path.splitext(audio_path)[0]}_stereo.wav'
+        command = shlex.split(f'ffmpeg -y -loglevel error -i "{audio_path}" -ac 2 -f wav "{stereo_path}"')
+        subprocess.run(command)
+        return stereo_path
+    else:
+        return audio_path
+
+def get_hash(filepath):
+    with open(filepath, 'rb') as f:
+        file_hash = hashlib.blake2b()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+
+    return file_hash.hexdigest()[:11]
+
+
+def preprocess_song(song_input, mdx_model_params, song_id,mdxnet_models_dir='mdxnet_models',output_dir='song_outputs', input_type='yt'):
+    keep_orig = False
+    if input_type == 'yt':
+        print('[~] Downloading song...')
+        song_link = song_input.split('&')[0]
+        orig_song_path = yt_download(song_link)
+    elif input_type == 'local':
+        orig_song_path = song_input
+        keep_orig = True
+    else:
+        orig_song_path = None
+
+    song_output_dir = os.path.join(output_dir, song_id)
+    if not os.path.exists(os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals.wav")) and not os.path.exists(os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Instrumental.wav")):
+        orig_song_path = convert_to_stereo(orig_song_path)
+        print('[~] Separating Vocals from Instrumental...')
+        vocals_path, instrumentals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR-MDX-NET-Voc_FT.onnx'), orig_song_path, denoise=True, keep_orig=keep_orig)
+    else:
+        vocals_path, instrumentals_path=os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals.wav") , os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Instrumental.wav")
+    if not os.path.exists(os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals_Main.wav")) and not os.path.exists(os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals_Backup.wav")):
+        print('[~] Separating Main Vocals from Backup Vocals...')
+        backup_vocals_path, main_vocals_path = run_mdx(mdx_model_params, song_output_dir, os.path.join(mdxnet_models_dir, 'UVR_MDXNET_KARA_2.onnx'), vocals_path, suffix='Backup', invert_suffix='Main', denoise=True)
+    else:
+        main_vocals_path, backup_vocals_path=os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals_Main.wav") , os.path.join(f"{output_dir}/{song_id}",f"{orig_song_path.replace('.mp3','')}_Vocals_Backup.wav")
+    return orig_song_path, vocals_path, instrumentals_path, main_vocals_path, backup_vocals_path
+
+def combine_audio(audio_paths, output_path, main_gain=0, backup_gain=0, inst_gain=0, output_format='mp3'):
+    main_vocal_audio = AudioSegment.from_wav(audio_paths[0]) - 4 + main_gain
+    backup_vocal_audio = AudioSegment.from_wav(audio_paths[1]) - 6 + backup_gain
+    instrumental_audio = AudioSegment.from_wav(audio_paths[2]) - 7 + inst_gain
+    main_vocal_audio.overlay(backup_vocal_audio).overlay(instrumental_audio).export(output_path, format=output_format)
+
+def yt_download(link):
+    ydl_opts = {
+        'format': 'bestaudio',
+        'outtmpl': '%(title)s',
+        'nocheckcertificate': True,
+        'ignoreerrors': True,
+        'no_warnings': True,
+        'quiet': True,
+        'extractaudio': True,
+        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(link, download=True)
+        download_path = ydl.prepare_filename(result, outtmpl='%(title)s.mp3')
+
+    return download_path
+
+def get_youtube_video_id(url, ignore_playlist=True):
+    """
+    Examples:
+    http://youtu.be/SA2iWivDJiE
+    http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    http://www.youtube.com/embed/SA2iWivDJiE
+    http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(url)
+    if query.hostname == 'youtu.be':
+        if query.path[1:] == 'watch':
+            return query.query[2:]
+        return query.path[1:]
+
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        if not ignore_playlist:
+            # use case: get playlist id not current video in playlist
+            with suppress(KeyError):
+                return parse_qs(query.query)['list'][0]
+        if query.path == '/watch':
+            return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/':
+            return query.path.split('/')[1]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+
+    # returns None for invalid YouTube url
+    return None
